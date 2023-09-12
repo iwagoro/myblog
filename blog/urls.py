f38cfd2410f31e0f001d84f53d33c228211d6489
rockwell <iwagoro733@gmail.com>
2023-09-12 13:23:55 +0900

    added post detail page

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.post_list,name='post_list'),  #views.post_listをルートURLに割り当てる
+    path('post/<int:pk>/',views.post_detail,name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+
 from .models import Post
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 
 # Create your views here.
@@ -7,3 +8,6 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') #今日以前に公開された記事を取得し、公開日時の昇順で並び替える
     return render(request,'blog/post_list.html',{'posts':posts}) #requestはユーザーが要求する全ての情報を持っているオブジェクト、{}はテンプレートに渡すコンテキスト(辞書型変数)
 
+def post_detail(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    return render(request,'blog/post_detail.html',{'post':post})
